85df30cee9626651e910fbcef40e49c782f3ff9d
Kristjan Lääne <xonproject@gmail.com>
2025-02-07 16:49:39 +0200

    basics

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { z } from 'zod';
 
@@ -11,6 +12,12 @@ const formSchema = z.object({
 type FormFields = z.infer<typeof formSchema>;
 
 export default function App() {
+  const [formValues, setFormValues] = useState({
+    name: '',
+    email: '',
+    phoneNumber: '',
+  });
+
   const {
     register,
     handleSubmit,
@@ -25,7 +32,7 @@ export default function App() {
   });
 
   const onSubmit: SubmitHandler<FormFields> = (data) => {
-    console.log(data);
+    setFormValues(data);
   };
 
   return (
@@ -33,26 +40,36 @@ export default function App() {
       <h1>
         CV Maker <span className='h1-span'>PRO</span>
       </h1>
-      <form className='form' onSubmit={handleSubmit(onSubmit)}>
-        <h2>Basic Information</h2>
-        <input {...register('name')} type='text' placeholder='Name' />
-        {errors.name && <div className='error'>{errors.name.message}</div>}
-        <input {...register('email')} type='email' placeholder='Email' />
-        {errors.email && <div className='error'>{errors.email.message}</div>}
+      <div className='page'>
+        <form className='form' onSubmit={handleSubmit(onSubmit)}>
+          <h2>Basic Information</h2>
+          <input {...register('name')} type='text' placeholder='Name' />
+          {errors.name && <div className='error'>{errors.name.message}</div>}
+          <input {...register('email')} type='email' placeholder='Email' />
+          {errors.email && <div className='error'>{errors.email.message}</div>}
 
-        <input
-          {...register('phoneNumber')}
-          type='text'
-          placeholder='Phone Number'
-        />
-        {errors.phoneNumber && (
-          <div className='error'>{errors.phoneNumber.message}</div>
-        )}
+          <input
+            {...register('phoneNumber')}
+            type='text'
+            placeholder='Phone Number'
+          />
+          {errors.phoneNumber && (
+            <div className='error'>{errors.phoneNumber.message}</div>
+          )}
 
-        <button type='submit' className='submit-btn'>
-          Submit
-        </button>
-      </form>
+          <button type='submit' className='submit-btn'>
+            {formValues.name === '' ? 'Submit' : 'Edit'}
+          </button>
+        </form>
+        <div className='preview'>
+          <div className='preview-basic-info'>
+            <h3 className='preview-header'>CV - {formValues.name}</h3>
+            <p className='preview-value name'>{formValues.name}</p>
+            <p className='preview-value email'>{formValues.email}</p>
+            <p className='preview-value phone'>{formValues.phoneNumber}</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
